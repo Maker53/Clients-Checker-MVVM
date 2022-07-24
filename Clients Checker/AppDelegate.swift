5546cdbb7fccd2211ed2461502da17579a247245
Maker <s.shalgin@gmail.com>
2022-07-24 14:09:08 +0300

    Initial Commit

---
+++ Clients Checker/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Clients Checker
+//
+//  Created by Станислав on 24.07.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
