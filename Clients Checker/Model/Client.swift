3c86ccc986544ccb343fa5daaeee91ce29f5d20b
Maker <s.shalgin@gmail.com>
2022-07-30 10:58:40 +0300

    refactor model

--- Clients Checker/Model/Client.swift
+++ Clients Checker/Model/Client.swift
@@ -7,17 +7,11 @@
 
 import RealmSwift
 
-class Client: Object {
+class Client: Object, ObjectKeyIdentifiable {
     
-    @Persisted var clientName: String
-    @Persisted var location: String
-    @Persisted var visitDate: Date
+    @Persisted var clientName = ""
+    @Persisted var location = ""
+    @Persisted var visitDate = Date()
     @Persisted var isDone = false
-    
-    convenience init(clientName: String, location: String, visitDate: Date) {
-        self.init()
-        self.clientName = clientName
-        self.location = location
-        self.visitDate = visitDate
-    }
+    @Persisted(primaryKey: true) var id: ObjectId
 }
